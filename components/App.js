44ac45db2456ee410024ce8633e4407420386abb
Zimu An <zian@ucsd.edu>
2022-08-28 22:25:48 -0400

    First Commit around-frontend

---
+++ components/App.js
@@ -0,0 +1,29 @@
+import React, {useState} from "react";
+import TopBar from "./TopBar";
+import Main from "./Main";
+import {TOKEN_KEY} from "../constants";
+
+function App() {
+    const [isLoggedIn, setIsLoggedIn] = useState(localStorage.getItem(TOKEN_KEY) ?
+        true : false);
+    const logout = () => {
+        setIsLoggedIn(false);
+        localStorage.removeItem(TOKEN_KEY);
+    }
+
+    const loggedIn = (token) => {
+        if (token) {
+            localStorage.setItem(TOKEN_KEY, token);
+            setIsLoggedIn(true);
+        }
+    };
+
+    return (
+        <div className="App">
+            <TopBar isLoggedIn={isLoggedIn} handleLogout={logout} />
+            <Main isLoggedIn={isLoggedIn} handleLoggedIn={loggedIn} />
+        </div>
+    );
+}
+
+export default App;
